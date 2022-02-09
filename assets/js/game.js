2390bf7b0c352b2e6bf9de9c51c214afccc6cdb9
megan <mtpott23@gmail.com>
2022-02-08 19:20:37 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,6 +70,11 @@ var fight = function(enemyName) {
 }
 }
 
+var startGame = function() {
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 for(var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
     //let player know what round they're in, remember that arrays start at 0 so it needs to have 1 added to it!
@@ -85,5 +90,47 @@ for(var i = 0; i < enemyNames.length; i++) {
 } else {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
+       }
+    }
+    
+//after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+EndGame();
+};
+
+//function to end the entire game
+var EndGame = function() {
+    //if player is still alive, player wins!
+    if(playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    } else {
+        window.alert("You've lost your robot in battle.");
+    }
+    //ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if(playAgainConfirm) {
+        //restart the game
+        startGame();
+    } else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
 }
-}
+
+//start the game when the page loads
+startGame();
+
+//wrap the game logic in a startGame() function
+//when the player is defeated or there are no more enemies, call an endGame() function that 
+    //alerts the player's total stats
+    //asks the player if they want to play again
+    //if yes, call startGame() to restart the game
+//after the player skips or defeats an enemy (and there are still more robots to fight)
+    //ask the player if they want to "shop" 
+    //if no, continue as normal
+    //if yes, call the shop() function
+    //in the shop() function, ask player if they want to "refill" health, "upgrade" attack, or "leave" the shop
+    //if refill, subtract money points from player and increase health
+    //if upgrade, subtract money points from player and increase attack power
+    //if leave, alert goodbye and exit the function
+    //any other invalid option, call shop() again
+
